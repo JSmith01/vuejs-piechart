1173269f246525cf95e0686098379933e766056d
Dmitry Zlygin <dmitry.zlygin@ascom.com>
2017-12-15 19:06:19 +0300

    changed and simplified structure; code reformatted

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <PieChart/>
+  <div>
+    <PieChart :data="data" :diameter="120" />
+    <div>
+      <button @click="generate">Generate new data</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,9 +12,21 @@ import PieChart from './PieChart';
 
 export default {
   name: 'app',
+
   components: { PieChart },
-  data () {
-    return {}
+
+  mounted() {
+    this.generate();
+  },
+
+  data() {
+    return { data: [] }
+  },
+
+  methods: {
+    generate() {
+      this.data = Array.from({ length: Math.floor(Math.random() * 6 + 2) }, () => Math.floor(Math.random() * 100 + 1));
+    }
   }
 }
 </script>
